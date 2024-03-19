59b614e8b92ec331919967c8bb3fbf80960bf468
haloooloolo <03_sharks.guises@icloud.com>
2024-03-19 03:27:32 +0100

    fix reentrancy bug

--- src/client.py
+++ src/client.py
@@ -1,19 +1,25 @@
 import discord
 
 import logging
-from typing import Any, Callable, Awaitable, Optional
+from typing import Any, Callable, Awaitable
+from threading import Lock
 
 
 class Client(discord.Client):
     def __init__(self, func: Callable[['Client'], Awaitable], args: Any):
         super().__init__()
         self.__func = func
+        self.__lock = Lock()
         self.args = args
 
     async def on_ready(self) -> None:
+        if not self.__lock.acquire(blocking=False):
+            return
+
         logging.info(f'Logged in as {self.user}')
         await self.__func(self)
         await self.close()
+        self.__lock.release()
 
     async def get_username(self, user_id: int) -> str:
         try:
